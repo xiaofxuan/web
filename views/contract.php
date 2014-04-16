8017d72a7378958246f7edc116924b09dbdb57d7
xiaofxuan <1377926472@qq.com>
2014-04-16 14:56:15 +0800

    空

--- views/contract.php
+++
@@ -1,7 +0,0 @@
-<!-- Modal -->
-  <div class="modal-body">
-    <p>中大之旅提醒您：在使用本网站的各项服务前，请认真阅读以下相关条例</p>
-  </div>
-  <div class="modal-footer">
-    <button class="btn btn-primary">已经阅读并同意该条例</button>
-  </div>
--- views/loading.php
+++
@@ -1,4 +0,0 @@
-<div class="text-center" style="height: 40em;line-height: 40em;overflow: hidden;">
-
-<i class="icon-spinner icon-spin icon-3x"></i>loading... 
-</div>
